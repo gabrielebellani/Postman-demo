f26411f80d25f6e0f9fe957d08a7a88db5b50949
Gabriele <sitehub.cloud@gmail.com>
2022-11-14 16:25:26 +0100

    Add new files

---
+++ resources/js/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="d-flex justify-content-center align-items-center">
+            <router-link to="/">Home</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
